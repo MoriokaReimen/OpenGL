44ea8d11c5e957e575c43a614044369c7656a9e8
Kei Nakata <menschenjager.mark.neun@gmail.com>
2015-04-16 06:06:32 +0900

    Add quartanion class

--- include/ObjectUtil.hpp
+++ include/ObjectUtil.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <GL/freeglut.h>
+#include <cmath>
+#define _USE_MATH_DEFINES
 
 class Point
 {
@@ -21,4 +23,61 @@ public:
     ObjectColor() = default;
     ObjectColor(GLfloat br, GLfloat bg, GLfloat bb) : r(br), g(bg), b(bb) {}
     GLfloat* toArray();
+};
+
+class Quaternion
+{
+  GLfloat w, x, y, z;
+public:
+  Quaternion()
+    : w(0.f), x(0.f), y(0.f), z(0.f)
+  {
+    return;
+  }
+
+  Quaternion(const GLfloat& bw, const GLfloat& bx, const GLfloat& by, const GLfloat& bz)
+    : w(bw), x(bx), y(by), z(bz)
+  {
+    return;
+  }
+
+  virtual ~Quaternion() = default;
+
+  void set(const GLfloat& bw, const GLfloat& bx, const GLfloat& by, const GLfloat& bz)
+  {
+    w = bw;
+    x = bx;
+    y = by;
+    z = bz;
+    return;
+  }
+
+  /* implement someday
+  void toMat(GLfloat* mat)
+  {
+    mat[0] = w * w + x * x - y * y - z * z;
+    mat[1] = 2 * (x * y + w * z);
+    mat[2] = 2 * (x * z - w * y);
+    mat[3] = 2 * (x * z - w * y);
+  }
+  */
+
+  void toAngleAxis(GLfloat& bx, GLfloat& by, GLfloat& bz, GLfloat& angle)
+  {
+    angle = 2 * std::acos(this->w) * 180 / M_PI;
+    GLfloat s = 1 - this->w * this->w;
+    if(s > 0.001)
+    {
+      bx    = this->x / std::sqrt(1 - this->w * this->w);
+      bx    = this->y / std::sqrt(1 - this->w * this->w);
+      bx    = this->z / std::sqrt(1 - this->w * this->w);
+      return;
+    }
+    else {
+      bx = 1;
+      by = 0;
+      bz = 0;
+      return;
+    }
+  }
 };
